59ac0e40d95bc8b5139e9bfc8d63b3a13aa240dd
EnriqueRosalesPerez <EnriqueRosalesPerez@192.168.118.133>
2018-11-12 19:36:02 +0100

    Incluido Javadoc de DAOException

--- ProyectoFinal/src/es/enriquerosales/enciclopedia/modelo/dao/DAOException.java
+++ ProyectoFinal/src/es/enriquerosales/enciclopedia/modelo/dao/DAOException.java
@@ -1,13 +1,32 @@
 package es.enriquerosales.enciclopedia.modelo.dao;
 
+/**
+ * Excepción que encapsula todos los errores que se puedan producir al trabajar
+ * con una interfaz DAO.
+ * 
+ * @author Enrique Rosales
+ *
+ */
 public class DAOException extends Exception {
 
 	private static final long serialVersionUID = -11903154423535696L;
 
+	/**
+	 * Constructor con mensaje.
+	 * 
+	 * @param msg
+	 *            El mensaje de la excepción.
+	 */
 	public DAOException(String msg) {
 		super(msg);
 	}
 
+	/**
+	 * Constructor con excepción.
+	 * 
+	 * @param ex
+	 *            La excepción a encapsular.
+	 */
 	public DAOException(Exception ex) {
 		super(ex);
 	}
