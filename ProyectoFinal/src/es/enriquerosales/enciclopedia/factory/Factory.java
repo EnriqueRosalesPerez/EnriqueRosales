8f6798418b5640c57e6f090c7a60bf19f3c0e6dd
EnriqueRosalesPerez <EnriqueRosalesPerez@users.noreply.github.com>
2018-11-13 18:33:53 +0100

    Creada clase Factory

---
+++ ProyectoFinal/src/es/enriquerosales/enciclopedia/factory/Factory.java
@@ -0,0 +1,53 @@
+package es.enriquerosales.enciclopedia.factory;
+
+import es.enriquerosales.enciclopedia.modelo.dao.DirectorioDAO;
+import es.enriquerosales.enciclopedia.modelo.dao.PersonajeDAO;
+import es.enriquerosales.enciclopedia.modelo.dao.UsuarioDAO;
+import es.enriquerosales.enciclopedia.modelo.dao.jdbc.DirectorioDAOJDBC;
+import es.enriquerosales.enciclopedia.modelo.dao.jdbc.JDBCDataSource;
+import es.enriquerosales.enciclopedia.modelo.dao.jdbc.PersonajeDAOJDBC;
+import es.enriquerosales.enciclopedia.modelo.dao.jdbc.UsuarioDAOJDBC;
+
+public class Factory {
+
+	private static UsuarioDAO usuarioDAO;
+	private static DirectorioDAO directorioDAO;
+	private static PersonajeDAO personajeDAO;
+	private static JDBCDataSource dataSource;
+
+	public static UsuarioDAO getUsuarioDAO() throws ClassNotFoundException {
+		if (usuarioDAO == null) {
+			UsuarioDAOJDBC bean = new UsuarioDAOJDBC();
+			bean.setDataSource(getJDBCDataSource());
+			usuarioDAO = bean;
+		}
+		return usuarioDAO;
+	}
+	
+	public static DirectorioDAO getDirectorioDAO() throws ClassNotFoundException {
+		if (usuarioDAO == null) {
+			DirectorioDAOJDBC bean = new DirectorioDAOJDBC();
+			bean.setDataSource(getJDBCDataSource());
+			directorioDAO = bean;
+		}
+		return directorioDAO;
+	}
+	
+	public static PersonajeDAO getPersonajeDAO() throws ClassNotFoundException {
+		if (personajeDAO == null) {
+			PersonajeDAOJDBC bean = new PersonajeDAOJDBC();
+			bean.setDataSource(getJDBCDataSource());
+			personajeDAO = bean;
+		}
+		return personajeDAO;
+	}
+
+	private static JDBCDataSource getJDBCDataSource() throws ClassNotFoundException {
+		if (dataSource == null) {
+			JDBCDataSource bean = new JDBCDataSource();
+			dataSource = bean;
+		}
+		return dataSource;
+	}
+
+}
