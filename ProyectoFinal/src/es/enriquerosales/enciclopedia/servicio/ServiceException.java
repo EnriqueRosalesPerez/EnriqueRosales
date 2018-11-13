4fd3630996f15faa60d5083d69856b1cfdc795b1
EnriqueRosalesPerez <EnriqueRosalesPerez@users.noreply.github.com>
2018-11-13 19:51:20 +0100

    Añadido Javadoc a ServiceException

--- ProyectoFinal/src/es/enriquerosales/enciclopedia/servicio/ServiceException.java
+++ ProyectoFinal/src/es/enriquerosales/enciclopedia/servicio/ServiceException.java
@@ -1,13 +1,32 @@
 package es.enriquerosales.enciclopedia.servicio;
 
+/**
+ * Excepción que encapsula todos los errores que se puedan producir al trabajar
+ * con la capa de servicio.
+ * 
+ * @author Enrique Rosales
+ *
+ */
 public class ServiceException extends Exception {
 
 	private static final long serialVersionUID = 3160769560075194610L;
-	
+
+	/**
+	 * Constructor con mensaje.
+	 * 
+	 * @param msg
+	 *            El mensaje de la excepción.
+	 */
 	public ServiceException(String msg) {
 		super(msg);
 	}
-	
+
+	/**
+	 * Constructor con excepción.
+	 * 
+	 * @param ex
+	 *            La excepción a encapsular.
+	 */
 	public ServiceException(Exception ex) {
 		super(ex);
 	}
