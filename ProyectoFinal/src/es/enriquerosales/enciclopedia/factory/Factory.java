e7ef621614b8222630dd12c482660c6c767c7abb
EnriqueRosalesPerez <EnriqueRosalesPerez@users.noreply.github.com>
2018-11-13 19:33:55 +0100

    Añadido Javadoc a la clase Factory

--- ProyectoFinal/src/es/enriquerosales/enciclopedia/factory/Factory.java
+++ ProyectoFinal/src/es/enriquerosales/enciclopedia/factory/Factory.java
@@ -8,6 +8,13 @@ import es.enriquerosales.enciclopedia.modelo.dao.jdbc.JDBCDataSource;
 import es.enriquerosales.enciclopedia.modelo.dao.jdbc.PersonajeDAOJDBC;
 import es.enriquerosales.enciclopedia.modelo.dao.jdbc.UsuarioDAOJDBC;
 
+/**
+ * Clase que implementa el patrón Factory para generar instancias de varias
+ * clases.
+ * 
+ * @author Enrique Rosales
+ *
+ */
 public class Factory {
 
 	private static UsuarioDAO usuarioDAO;
@@ -15,6 +22,13 @@ public class Factory {
 	private static PersonajeDAO personajeDAO;
 	private static JDBCDataSource dataSource;
 
+	/**
+	 * Devuelve una instancia de {@link UsuarioDAO}.
+	 * 
+	 * @return Una instancia de {@link UsuarioDAO}.
+	 * @throws ClassNotFoundException
+	 *             Si se produce un error al inyectar dependencias.
+	 */
 	public static UsuarioDAO getUsuarioDAO() throws ClassNotFoundException {
 		if (usuarioDAO == null) {
 			UsuarioDAOJDBC bean = new UsuarioDAOJDBC();
@@ -23,7 +37,14 @@ public class Factory {
 		}
 		return usuarioDAO;
 	}
-	
+
+	/**
+	 * Devuelve una instancia de {@link DirectorioDAO}.
+	 * 
+	 * @return Una instancia de {@link DirectorioDAO}.
+	 * @throws ClassNotFoundException
+	 *             Si se produce un error al inyectar dependencias.
+	 */
 	public static DirectorioDAO getDirectorioDAO() throws ClassNotFoundException {
 		if (directorioDAO == null) {
 			DirectorioDAOJDBC bean = new DirectorioDAOJDBC();
@@ -32,7 +53,14 @@ public class Factory {
 		}
 		return directorioDAO;
 	}
-	
+
+	/**
+	 * Devuelve una instancia de {@link PersonajeDAO}.
+	 * 
+	 * @return Una instancia de {@link PersonajeDAO}.
+	 * @throws ClassNotFoundException
+	 *             Si se produce un error al inyectar dependencias.
+	 */
 	public static PersonajeDAO getPersonajeDAO() throws ClassNotFoundException {
 		if (personajeDAO == null) {
 			PersonajeDAOJDBC bean = new PersonajeDAOJDBC();
@@ -42,6 +70,13 @@ public class Factory {
 		return personajeDAO;
 	}
 
+	/**
+	 * Devuelve una instancia de {@link JDBCDataSource}.
+	 * 
+	 * @return Una instancia de {@link JDBCDataSource}.
+	 * @throws ClassNotFoundException
+	 *             Si no se encuentra el Driver JDBC.
+	 */
 	private static JDBCDataSource getJDBCDataSource() throws ClassNotFoundException {
 		if (dataSource == null) {
 			JDBCDataSource bean = new JDBCDataSource();
